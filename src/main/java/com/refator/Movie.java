93037b2d553f827f5fdda36e2a5a79ceed60c990
chenwenbo <594017622@qq.com>
2016-08-15 10:26:53 +0800

    init project

---
+++ src/main/java/com/refator/Movie.java
@@ -0,0 +1,19 @@
+package com.refator;
+
+/**
+ * chao.yu@dianping.com
+ * Created by apple on 2016/08/12 17:27.
+ */
+public class Movie {
+    public static final int CHILDRENS = 2;
+    public static final int REGULAR = 0;
+    public static final int NEW_RELEASE = 1;
+
+    private String _title;
+    private int _priceCode;
+
+    public Movie(){
+
+    }
+
+}
