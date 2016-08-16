e3d012df5b35dc89e51791678fa0df1130d4b2fc
chenwenbo <594017622@qq.com>
2016-08-16 17:44:12 +0800

    replace temp with query

--- src/main/java/com/refator/Customer.java
+++ src/main/java/com/refator/Customer.java
@@ -21,24 +21,39 @@ public class Customer {
     }
 
     public String statement(){
-        double totalAmount = 0;
-        int frequentRenterPoints = 0; //point
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for "+ getName() + "\n";
         while (rentals.hasMoreElements()){
             Rental each = (Rental) rentals.nextElement();
 
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
                     String.valueOf(each.getCharge()) + "\n";
-            totalAmount += each.getCharge();
         }
         // add footer lines
-        result += "Amount owed is" + String.valueOf(totalAmount) + "\n";
-        result += "You earn" + String.valueOf(frequentRenterPoints) + "frequent renter points";
+        result += "Amount owed is" + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earn" + String.valueOf(getFrequentRenterPoints()) + "frequent renter points";
         return result;
+    }
+
+    private int getFrequentRenterPoints(){
+        int frequentRenterPoints = 0; //point
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()){
+            Rental each = (Rental) rentals.nextElement();
+            frequentRenterPoints += each.getFrequentRenterPoints();
+        }
+        return frequentRenterPoints;
+    }
+
+    private double getTotalCharge(){
+        double totalAmount = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()){
+            Rental each = (Rental) rentals.nextElement();
+            totalAmount += each.getCharge();
+        }
+        return totalAmount;
     }
 
 }
