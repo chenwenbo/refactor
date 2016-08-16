1c6c98b139ce8e709864fd5489cee0212d7e5461
chenwenbo <594017622@qq.com>
2016-08-16 18:51:24 +0800

    加入多态进行处理

---
+++ src/main/java/com/refator/NewReleasePrice.java
@@ -0,0 +1,12 @@
+package com.refator;
+
+public class NewReleasePrice extends Price{
+
+    int getPriceCode() {
+        return Movie.NEW_RELEASE;
+    }
+
+
+    double getCharge(int daysRented){
+        return daysRented * 3;
+    }}
